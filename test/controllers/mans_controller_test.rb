de92cc2073fae66afaef1315e2a4981aebb0bbd0
sc30gsw <haidoushi875@gmail.com>
2021-01-28 16:06:49 +0900

    Rubocopの実行

--- test/controllers/mans_controller_test.rb
+++ test/controllers/mans_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class MansControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get mans_index_url
     assert_response :success
   end
-
 end
