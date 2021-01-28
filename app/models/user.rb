b86b0585b7e22650ba91703db337212d3f693d00
sc30gsw <haidoushi875@gmail.com>
2021-01-28 16:10:01 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  valisates :nickname, presence: true
+  validates :nickname, presence: true
 end
