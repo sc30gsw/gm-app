ecdb6d66b411e22c415e5e86bba3fda1ce2e4308
sc30gsw <haidoushi875@gmail.com>
2021-02-01 14:31:55 +0900

    ユーザー詳細ページへのパスの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
   root to: 'mans#index'
+  resources :users, only: [:show]
 end
