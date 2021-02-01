d413f982588b39733f09393d7bf2863b6213d1e7
sc30gsw <haidoushi875@gmail.com>
2021-02-01 15:16:35 +0900

    登録済みか判定

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,13 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
-     nickname: auth.info.name,
-       email: auth.info.email
-   )
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
   end
 end
