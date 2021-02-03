9f83405556f717c1a536bbf0d0324ba2e6d8b767
sc30gsw <haidoushi875@gmail.com>
2021-02-03 10:12:51 +0900

    intorosテーブルの作成

--- db/migrate/20210203010804_create_intros.rb
+++ db/migrate/20210203010804_create_intros.rb
@@ -1,7 +1,9 @@
 class CreateIntros < ActiveRecord::Migration[6.0]
   def change
     create_table :intros do |t|
-
+      t.text :profile
+      t.string :website
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_060055) do
+ActiveRecord::Schema.define(version: 2021_02_03_010804) do
+
+  create_table "intros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "profile"
+    t.string "website"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_intros_on_user_id"
+  end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -34,5 +43,6 @@ ActiveRecord::Schema.define(version: 2021_02_01_060055) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "intros", "users"
   add_foreign_key "sns_credentials", "users"
 end
