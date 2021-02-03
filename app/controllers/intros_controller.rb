3734042c32207d9f49fa8a60f6161e3fdf35bac2
sc30gsw <haidoushi875@gmail.com>
2021-02-03 10:23:47 +0900

    アクション内の処理の設定

--- app/controllers/intros_controller.rb
+++ app/controllers/intros_controller.rb
@@ -1,15 +1,38 @@
 class IntrosController < ApplicationController
+  before_action :set_intro, only: [:edit, :update]
 
   def new
+    @intro = Intro.new
+    redirect_to user_path(current_user.id) if Intro.find_by(user_id: current_user.id)
   end
 
   def create
+    @intro = Intro.new(intro_params)
+    if @intro.save
+      redirect_to user_path(@intro.user.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    unless current_user.id == @intro.user_id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
+    if @intro.update(intro_params)
+      redirect_to user_path(@intro.user.id)
+    else
+      render :edit
+    end
   end
-  
+
+  private
+
+  def set_intro
+    @intro = Intro.find(params[:id])
+  end
+
 end
