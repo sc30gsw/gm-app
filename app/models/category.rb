6363a996cdb18218da094b05797159061176545b
sc30gsw <haidoushi875@gmail.com>
2021-02-04 12:41:53 +0900

    ActiveHashカテゴリーの作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,33 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    {id: 1, name: "--"},
+    {id: 2, name: "縄文時代"}
+    {id: 3, name: "弥生時代"},
+    {id: 4, name: "古墳時代"},
+    {id: 5, name: "飛鳥時代"},
+    {id: 6, name: "奈良時代"},
+    {id: 7, name: "平安時代"},
+    {id: 8, name: "鎌倉時代"},
+    {id: 9, name: "室町時代"},
+    {id: 10, name: "安土桃山時代"},
+    {id: 11, name: "江戸時代"},
+    {id: 12, name: "明治時代"},
+    {id: 13, name: "大正時代"},
+    {id: 14, name: "昭和時代"},
+    {id: 15, name: "イタリア"},
+    {id: 16, name: "イギリス"},
+    {id: 17, name: "フランス"},
+    {id: 18, name: "スペイン"},
+    {id: 19, name: "ドイツ"},
+    {id: 20, name: "ロシア"},
+    {id: 21, name: "アメリカ"},
+    {id: 22, name: "トルコ"},
+    {id: 23, name: "エジプト"},
+    {id: 24, name: "インド"},
+    {id: 25, name: "中東"},
+    {id: 26, name: "中国"},
+    {id: 27, name: "現代"}
+  ]
+
+  
 end
