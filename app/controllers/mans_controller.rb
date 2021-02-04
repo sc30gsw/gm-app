366732f9d0fc4b9fa898e61f54bbe4586785573a
sc30gsw <haidoushi875@gmail.com>
2021-02-04 12:55:45 +0900

    アクション内の処理の決定

--- app/controllers/mans_controller.rb
+++ app/controllers/mans_controller.rb
@@ -3,8 +3,16 @@ class MansController < ApplicationController
   end
 
   def new
+    @man = Man.new
   end
 
   def create
+    @man = Man.new(man_params)
+    if @man.valid?
+      @man.save
+      redirect_to :root_path
+    else
+      render :new
+    end
   end
 end
