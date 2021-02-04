072fe1788d2f4157c03abce01178a03a69a0d193
sc30gsw <haidoushi875@gmail.com>
2021-02-04 12:21:22 +0900

    テーブルの作成

---
+++ db/migrate/20210204031758_create_men.rb
@@ -0,0 +1,14 @@
+class CreateMen < ActiveRecord::Migration[6.0]
+  def change
+    create_table :men do |t|
+      t.string :name, null: false
+      t.text :content
+      t.integer :category_id, null: false
+      t.string :adress, null: false
+      t.float :latitude, null: false
+      t.float :longtude, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
