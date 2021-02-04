b00d464946705e43f2c8c3570dbc8b6399a15ef0
sc30gsw <haidoushi875@gmail.com>
2021-02-04 12:21:33 +0900

    不要ファイルの削除

--- db/migrate/20210204021117_create_men.rb
+++
@@ -1,8 +0,0 @@
-class CreateMen < ActiveRecord::Migration[6.0]
-  def change
-    create_table :men do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/factories/man.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :man do
-  end
-end
