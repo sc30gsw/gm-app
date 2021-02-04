7291d8287e0a5927e2e89c0a009731cf6a6a9331
sc30gsw <haidoushi875@gmail.com>
2021-02-04 12:45:41 +0900

    バリデーションの設定

--- app/models/man.rb
+++ app/models/man.rb
@@ -3,4 +3,13 @@ class Man < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   belongs_to :category
+
+  validates :category_id, numericality: {other_than: 1}
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :address
+    validates :latitude
+    validates :longtude
+  end
 end
