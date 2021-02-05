ac16950db20498c15a5b2cdbd8ffcd2d7e186818
sc30gsw <haidoushi875@gmail.com>
2021-02-05 10:06:57 +0900

    Googleマップの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 require('jquery')
 require("./icon_preview")
 require("./map")
+//= require underscore
+//= require gmaps/google
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
