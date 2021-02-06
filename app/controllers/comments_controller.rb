f91cd2e4e7da2f4ec35076efce317c4a196ef8b7
sc30gsw <haidoushi875@gmail.com>
2021-02-07 06:27:36 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:man).permit(:text).merge(user_id: current_user.id, man_id: params[:man_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, man_id: params[:man_id])
   end
 end
