45d85795396b3d5f6df2dee0a729d8fd1a56987e
sc30gsw <haidoushi875@gmail.com>
2021-02-07 06:37:58 +0900

    Commentモデルの単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント新規投稿' do
+    context 'コメント新規投稿がうまくいくとき' do
+      it 'すべての項目が存在すれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント新規投稿がうまくいかないとき' do
+      it 'textが空だと保存できない' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Textを入力してください')
+      end
+    end
+  end
 end
