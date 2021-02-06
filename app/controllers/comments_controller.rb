0558030349810ee79c44ec77fec67418eefff9b0
sc30gsw <haidoushi875@gmail.com>
2021-02-07 06:08:10 +0900

    アクション内の処理の設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
+
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to man_path(@comment.man)
+    else
+      @man = @comment.man
+      @comments = @man.comments
+      render 'man/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:man).permit(:text).merge(user_id: current_user.id, man_id: params[:man_id])
   end
 end
