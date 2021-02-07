5a2393cd5decb8a931a8370952dc630ed31ea52f
sc30gsw <haidoushi875@gmail.com>
2021-02-07 14:34:51 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :intros, only: [:new, :create, :edit, :update]
   resources :mans, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
   get '/mans/category/:id', to: 'mans#category'
 end
