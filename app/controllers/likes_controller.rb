dc86557d09611209122d0ab220d89e71717f37f4
sc30gsw <haidoushi875@gmail.com>
2021-02-07 14:40:41 +0900

    アクション内の処理追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,26 @@
 class LikesController < ApplicationController
+  before_action :set_man
 
   def create
     @like = Like.create(user_id: current_user.id, man_id: params[:man_id])
+    if @like.save
+      redirect_to man_path(@man.id)
+    else
+      render "mans/show"
   end
   
   def destroy
     @like = Like.find_by(user_id: current_user.id, man_id: params[:man_id])
-    @like.destroy
+    if @like.destroy
+      redirect_to man_path(@man.id)
+    else
+      render "mans/show"
   end
-  
+
+  private 
+
+  def set_man
+    @man = Man.find(params[:man_id])
+  end
+
 end
