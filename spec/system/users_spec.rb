f8f458f36e9f61f963820aaa270a502a2cc15dad
sc30gsw <haidoushi875@gmail.com>
2021-02-08 10:20:48 +0900

    ユーザー新規登録の結合テスト(正常系)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "新規登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.build(:user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにユーザーのドロップダウンメニューがあることを確認する
+      expect(page).to have_selector('.btn-header-drop')
+      # ドロップダウンメニューをクリックする
+      find('.btn-header-drop').click
+      # サインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('登録する')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change{ User.count }.by(1)
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # ドロップダウンメニューをクリックする
+      find('.btn-header-drop').click
+      # ログアウトボタンが表示されることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('登録する')
+      expect(page).to have_no_content('ログインする')
+    end
+  end
+
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+    end
+  end
 end
