0e0e1f21b9a557d5e54f4529fc42a38165ab3712
sc30gsw <haidoushi875@gmail.com>
2021-02-08 09:19:42 +0900

    テストデータの作成

--- spec/factories/intros.rb
+++ spec/factories/intros.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :intro do
+    profile {Faker::Lorem.sentence}
+    website {Faker::Lorem.sentence}
+
+    association :user
+    
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
