7f1f8c5244253fce38e5b0a474e7f2577d9d63d3
sc30gsw <haidoushi875@gmail.com>
2021-02-10 10:18:03 +0900

    修正

--- spec/factories/men.rb
+++ spec/factories/men.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     content { Faker::Lorem.sentence }
     category_id { 2 }
-    address { Faker::Address.name }
+    address { 'ニューヨーク' }
     latitude { 180 }
     longitude { 180 }
 
@@ -11,6 +11,7 @@ FactoryBot.define do
 
     after(:build) do |product|
       product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      product.image.attach(io: File.open('public/images/test_image2.png'), filename: 'test_image2.png')
     end
   end
 end
