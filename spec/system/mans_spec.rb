320f3b5a0d714169ff114836475138360e1220a2
sc30gsw <haidoushi875@gmail.com>
2021-02-10 10:18:13 +0900

    新規投稿の結合テスト

--- spec/system/mans_spec.rb
+++ spec/system/mans_spec.rb
@@ -1,9 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "Mans", type: :system do
+RSpec.describe "新規投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @man = FactoryBot.build(:man)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '新規投稿がうまくいくとき' do
+    it 'ログインしたユーザーは新規投稿できる' do
+      # ログインする
+      sign_in(@user)
+      # トップページにユーザーのドロップダウンメニューがあることを確認する
+      expect(page).to have_selector('.btn-header-drop')
+      # ドロップダウンメニューをクリックする
+      find('.btn-header-drop').click
+      # 新規投稿ページに遷移するボタンがあることを確認する
+      expect(page).to have_content('投稿する')
+      # 投稿ページに遷移する
+      visit new_man_path
+      # フォームに情報を入力する
+      fill_in 'man_name', with: @man.name
+      find("#man_category_id").find("option[value='2']").select_option
+      fill_in 'address', with: @man.address
+      fill_in 'man_content', with: @man.content
+      # 添付する画像を定義する
+      image_path = Rails.root.join('public/images/test_image.png')
+      # 画像選択フォームに画像を添付する
+      attach_file('man[image]', image_path, make_visible: true)
+      # 送信するとManモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Man.count }.by(1)
+      # トップページに遷移することを確認する
+      expect(current_path).to eq root_path
+      # トップページは先ほど投稿した内容(name)が存在する
+      expect(page).to have_content(@man.name)
+    end
+  end
+
+  context '新規投稿がうまくいかないとき' do
+    it 'ログインしていないと新規投稿ページに遷移できない' do
+      # トップページに遷移する
+      visit root_path
+      # トップページにユーザーのドロップダウンメニューがあることを確認する
+      expect(page).to have_selector('.btn-header-drop')
+      # ドロップダウンメニューをクリックする
+      find('.btn-header-drop').click
+      # 新規投稿ページに遷移するボタンがないことを確認する
+      expect(page).to have_no_content('投稿する')
+    end
+  end
 end
