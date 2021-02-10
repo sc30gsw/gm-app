bc8e9e9c1136ba06f16ee3a347252457ac62383a
sc30gsw <haidoushi875@gmail.com>
2021-02-10 10:02:20 +0900

    編集用の画像を追加

--- spec/factories/intros.rb
+++ spec/factories/intros.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     
     after(:build) do |product|
       product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      product.image.attach(io: File.open('public/images/test_image2.png'), filename: 'test_image2.png')
     end
   end
 end
