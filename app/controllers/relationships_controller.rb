ab21f313d0f0ab5d693901ca2d6f34df9d5fde4b
sc30gsw <haidoushi875@gmail.com>
2021-02-13 08:14:19 +0900

    アクションの設定

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,32 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
 
   def create
+    following = current_user.follow(@user)
+    if following.save
+      flash[:success] = 'ユーザーをフォローしました'
+      redirect_back(fallback_location: user_path(@user))
+    else
+      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+      redirect_back(fallback_location: user_path(@user))
+    end
   end
 
   def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:success] = 'ユーザーのフォローを解除しました'
+      redirect_back(fallback_location: user_path(@user))
+    else
+      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+      redirect_back(fallback_location: user_path(@user))
+    end
   end
-  
+
+  private
+
+  def set_user
+    @user = User.find(params[:follow_id])
+  end
+
 end
