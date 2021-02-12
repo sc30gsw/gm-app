3cb7ae10dcd53076d65953bcde53b7df2bb3ba2b
sc30gsw <haidoushi875@gmail.com>
2021-02-13 08:19:56 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, dependent: :destroy
   has_many :liked_mans, through: :likes, source: :man
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationsihps, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationsihps, source: :user
