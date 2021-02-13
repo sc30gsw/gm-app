c9084e4e2386560ad6769fee370e3dca4382ffda
sc30gsw <haidoushi875@gmail.com>
2021-02-14 06:52:24 +0900

    降順設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.order(created_at: :DESC)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
