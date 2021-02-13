2f4b30fcf7b97d558b7b0984ce46ba69e5af2024
sc30gsw <haidoushi875@gmail.com>
2021-02-14 07:47:26 +0900

    通知の有無によるアイコンの表示切り替え

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.passive_notifications.order(created_at: :DESC)
+    @notifications = current_user.passive_notifications.where.not(visitor_id: current_user.id).order(created_at: :DESC)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
