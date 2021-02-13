d826fb1ce18a75e79a502cfcb5a2b04cca471c98
sc30gsw <haidoushi875@gmail.com>
2021-02-14 06:51:53 +0900

    アクションの設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,15 @@
 class NotificationsController < ApplicationController
 
   def index
+    @notifications = current_user.passive_notifications
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
   end
 
   def destroy
+    @notifications = current_user.passive_notifications.destroy_all
+    redirect_to root_path
   end
-  
+
 end
