03376813b8fd18338b1ee14919785e365aa6189c
sc30gsw <haidoushi875@gmail.com>
2021-02-19 09:54:51 +0900

    followアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def follow
+    @follow_user = User.find(params[:id])
+    @users = @follow_user.followings.order('relationships.created_at DESC')
+  end
 end
