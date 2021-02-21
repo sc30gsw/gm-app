a1c7e91b18f88170d114850bb85ac1b62e355a3b
sc30gsw <haidoushi875@gmail.com>
2021-02-22 06:35:54 +0900

    Roomsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
