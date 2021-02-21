278a04bbfbf17f0f8382ce8ac604f63af2f0fb69
sc30gsw <haidoushi875@gmail.com>
2021-02-22 07:06:14 +0900

    アクションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,14 @@
 class RoomsController < ApplicationController
-  def index
+  def create
+    room = Room.create
+    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    redirect_to room_path(room)
   end
 
   def show
+    @room = Room.find(params[:id])
+    @message = Message.new
+    @another_entry = @room.entries.find_by('user_id != ?', current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,23 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def show
+    @current_entry = Entry.where(user_id: current_user.id)
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |cu|
+        @another_entry.each do |au|
+          if cu.room_id == au.room_id
+            @is_room = true
+            @room_id = cu.room_id
+          end
+        end
+      end
+
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def follow
