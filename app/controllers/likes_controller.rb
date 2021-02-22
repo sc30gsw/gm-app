c7c1c05031c56917b6394ed07d6b5a8880870792
sc30gsw <haidoushi875@gmail.com>
2021-02-22 10:19:35 +0900

    各一覧からチャットルームへ遷移できるよう設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,6 +5,24 @@ class LikesController < ApplicationController
   def index
     @user = User.find(params[:id])
     @mans = @user.liked_mans.order('likes.created_at DESC')
+
+    @current_entry = Entry.where(user_id: current_user.id)
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |cu|
+        @another_entry.each do |au|
+          if cu.room_id == au.room_id
+            @is_room = true
+            @room_id = cu.room_id
+          end
+        end
+      end
+
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def create
