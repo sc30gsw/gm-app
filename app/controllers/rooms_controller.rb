a18b6325027751041089c233e003285e3be70776
sc30gsw <haidoushi875@gmail.com>
2021-02-22 10:19:52 +0900

    indexアクションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,11 +2,20 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_room, only: [:show, :destroy]
 
+  def index
+    @current_entries = current_user.entries
+    my_room_ids = []
+    @current_entries.each do |entry|
+      my_room_ids << entry.room.id
+    end
+    @another_entries = Entry.where(room_id: my_room_ids).where.not(user_id: current_user.id)
+  end
+
   def create
-    @room = Room.create
-    @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id)
-    redirect_to room_path(@room)
+    room = Room.create
+    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    redirect_to room_path(room)
   end
 
   def show
