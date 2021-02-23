eac80ef445b69f589ca36715f3b1cb17cab57ad2
sc30gsw <haidoushi875@gmail.com>
2021-02-24 06:38:18 +0900

    ログインの有無の記述の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   before_action :set_user
-  before_action :set_current_entry
-  before_action :set_another_entry
+  before_action :set_current_entry, except: [:show]
+  before_action :set_another_entry, except: [:show]
 
   def show
-    @current_entry = Entry.where(user_id: current_user.id)
-    @another_entry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @current_entry.each do |cu|
-        @another_entry.each do |au|
-          if cu.room_id == au.room_id
-            @is_room = true
-            @room_id = cu.room_id
+    if user_signed_in?
+      @current_entry = Entry.where(user_id: current_user.id)
+      @another_entry = Entry.where(user_id: @user.id)
+      unless @user.id == current_user.id
+        @current_entry.each do |cu|
+          @another_entry.each do |au|
+            if cu.room_id == au.room_id
+              @is_room = true
+              @room_id = cu.room_id
+            end
           end
         end
-      end
 
-      unless @is_room
-        @room = Room.new
-        @entry = Entry.new
+        unless @is_room
+          @room = Room.new
+          @entry = Entry.new
+        end
       end
     end
   end
