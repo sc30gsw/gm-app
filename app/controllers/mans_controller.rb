40bd3b36c40768e961d0c38f5724cdda75c472c9
sc30gsw <haidoushi875@gmail.com>
2021-02-26 11:40:01 +0900

    Rubocop修正

--- app/controllers/mans_controller.rb
+++ app/controllers/mans_controller.rb
@@ -68,7 +68,7 @@ class MansController < ApplicationController
   end
 
   def like
-   @mans = @man.liked_users.order('likes.created_at DESC').page(params[:page]).per(15)
+    @mans = @man.liked_users.order('likes.created_at DESC').page(params[:page]).per(15)
   end
 
   private
