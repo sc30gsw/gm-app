8cbff05b812e4cebb7aa0e67f370110456d8258b
sc30gsw <haidoushi875@gmail.com>
2021-02-27 10:30:14 +0900

    週間と合計の割合表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
 
   def active
     @status_mans = @user.mans.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
-    @all_count = @status_mans.count
+    @all_mans = @user.mans
   end
 
   private
